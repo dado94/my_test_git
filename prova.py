302e04d5aa249594c3e055e2e63e42d84ab98aea
Davide Baitelli <davide.baitelli94@gmail.com>
2022-07-20 01:31:12 +0200

    made some optimization and by ref logic

--- prova.py
+++ prova.py
@@ -1,12 +1,15 @@
 
 
 def main():
-	res = bubble_sort([6, 3, 5, 2, 7, 1])
-	print('Result: ' + str(res))
-	res = bubble_sort([])
-	print('Result: ' + str(res))
-	res = bubble_sort([5])
-	print('Result: ' + str(res))
+	my_array = [6, 3, 5, 2, 7, 1]
+	bubble_sort(my_array)
+	print('Result: ' + str(my_array))
+	my_array = []
+	bubble_sort(my_array)
+	print('Result: ' + str(my_array))
+	my_array = [6]
+	bubble_sort(my_array)
+	print('Result: ' + str(my_array))
 
 def swap_l(to_sort, ix):
 	tmp = to_sort[ix + 1]
@@ -14,18 +17,16 @@ def swap_l(to_sort, ix):
 	to_sort[ix] = tmp
 
 def bubble_sort(to_sort):
-	if len(to_sort) <= 1:
+	n = len(to_sort)
+	if n <= 1:
 		return to_sort
 	swapped = True
 	while swapped:
 		swapped = False
-		for ix in range(len(to_sort) - 1):
+		for ix in range(n - 1):
 			if to_sort[ix] > to_sort[ix + 1]:
 				swap_l(to_sort, ix)
-				
 				swapped = True				
-	return to_sort
-
 main()
 
 
